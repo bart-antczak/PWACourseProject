14c342a53dae9e57a0936dbea8e817ce41ca67eb
bart-mant-io <antczak.bar@gmail.com>
2019-02-11 20:58:47 +0100

    Dodanie dynamicznego cacheowania, versioning

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,11 @@
+var CACHE_STATIC_NAME = 'static-v4';
+var CACHE_DYNAMIC_NAME = 'dynamic-v2';
+
 self.addEventListener('install', function(event) {
     console.log('[Service Worker] Installing Service Worker ...', event);
     event.waitUntil(
-        caches.open('static')
+        /* Nadawanie wersji cacheowi */
+        caches.open(CACHE_STATIC_NAME)
             .then(function (cache) {
                 console.log('[SW] Precaching app shell');
                 cache.addAll([
@@ -18,15 +22,23 @@ self.addEventListener('install', function(event) {
                     'https://fonts.googleapis.com/css?family=Roboto:400,700',
                     'https://fonts.googleapis.com/icon?family=Material+Icons',
                     'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
-                ]);
-                cache.add('/index.html');
-                cache.add('/src/js/app.js');
+                ])
             })
     );
 });
 
 self.addEventListener('activate', function(event) {
     console.log('[Service Worker] Activating Service Worker ...', event);
+    event.waitUntil(
+        caches.keys()
+            .then(function (keyList) {
+                return Promise.all(keyList.map(function (key) {
+                    if (key !== CACHE_STATIC_NAME && CACHE_DYNAMIC_NAME) {
+                        return caches.delete(key)
+                    }
+                }));
+            })
+    );
     return self.clients.claim();
 });
 
@@ -38,6 +50,14 @@ self.addEventListener('fetch', function(event) {
                     return response;
                 } else {
                     return fetch(event.request)
+                        .then(function (res) {
+                            return caches.open(CACHE_DYNAMIC_NAME)
+                                .then(function (cahce) {
+                                    cache.put(event.request.url, res.clone());
+                                    return res;
+                                })
+                        })
+                        .catch()
                 }
             })
     );
