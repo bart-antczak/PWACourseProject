142ec31e31e6e88c162e9431bbccf3e93c799740
bart-mant-io <antczak.bar@gmail.com>
2019-02-15 14:52:38 +0100

    Push Notifications [2] - Przeniesienie na SW, dodanie actions i listenerów

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -24,10 +24,36 @@ window.addEventListener('beforeinstallprompt', function(event) {
 });
 
 function displayConfirmNotification() {
-    var options = {
-        body: 'You successfully subscribed to our Notification Service!'
-    };
-    new Notification('Successfully subscribed!', options);
+    if ('serviceWorker' in navigator) {
+        var options = {
+            body: 'You successfully subscribed to our Notification Service!',
+            icon: '/src/images/icons/app-icon-96x96.png',
+            image: '/src/image/sf-boat.jpg',
+            dir: 'ltr',
+            lang: 'en-US', // BCP 47
+            vibrate: [100, 50, 200], // Vibration
+            badge: '/src/images/icons/app-icon-96x96.png', // On Android
+            // ADVANCE OPTIONS
+            tag: 'confirm-notification',
+            renotify: true, // Inform many times
+            actions: [
+                {
+                    action: 'confirm',
+                    title: 'Okay',
+                    icon: '/src/images/icons/app-icon-96x96.png'
+                },
+                {
+                    action: 'cancel',
+                    title: 'Cancel',
+                    icon: '/src/images/icons/app-icon-96x96.png'
+                }
+            ]
+        };
+        navigator.serviceWorker.ready
+            .then(function (swreg) {
+                swreg.showNotification('Successfully subscribed! (from SW)', options);
+            })
+    }
 }
 
 function askForNotificationPermission() {
--- public/sw.js
+++ public/sw.js
@@ -220,3 +220,21 @@ self.addEventListener('sync', function(event) {
         );
     }
 });
+
+/* Listenery dla Notification */
+
+self.addEventListener('notificationclick', function (event) {
+   var notification = event.notification;
+   var action = event.action;
+   console.log(notification);
+   if (action === 'confirm') {
+       console.log('Confirm was chosen');
+       notification.close();
+   } else {
+       console.log(action);
+   }
+});
+
+self.addEventListener('notificationclose', function (event) {
+    console.log('Notification was closed', event);
+});
